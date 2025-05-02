24d84d254583148bf9028e86b0913e3d181e1d44
hans schilling <hasp1988@gmail.com>
2025-05-02 17:56:22 +0400

    added googleTagManager and og-image

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,4 @@
 export const googleCalendarUrl = process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_URL
 export const formspreeId = process.env.NEXT_PUBLIC_FORMSPREE_ID
+export const googleTagManagerUrl =
+  process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_URL
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,28 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
+import { googleTagManagerUrl } from './constants'
 
 export const metadata: Metadata = {
   title: 'Longlister',
   description: 'The Recruitment Revolution',
   icons: {
     icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'Longlister',
+    description: 'The Recruitment Revolution',
+    url: 'localhost:8080',
+    siteName: 'Longlister',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
 }
 
@@ -16,6 +33,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        {/* Google Analytics */}
+        <Script strategy='afterInteractive' src={googleTagManagerUrl} />
+        <Script
+          id='ga-init'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YB8L9N217V');
+            `,
+          }}
+        />
+      </head>
       <body className=''>{children}</body>
     </html>
   )
